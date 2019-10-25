ba530d65e01e2efe682a501f64c37cfe81b8a33d
RogerTorresA <55524395+RogerTorresA@users.noreply.github.com>
2019-10-25 01:00:27 -0500

    Update Examen C_1.m

--- Examen C_1.m
+++ Examen C_1.m
@@ -10,22 +10,20 @@ while(e(i) > 0.001)
     fxl=(xl^3)+4*(xl^2)-10
     fxr=(xr^3)+4*(xr^2)-10
     fxu=(xu^3)+4*(xu^2)-10
-    
-    if(fxl*fxr < 0)
+   
+  if(fxl*fxr < 0)
         xu=xr
         e(i+1)=abs(xl-xr);
     end
-    
     if(fxu*fxr < 0)
         xl=xr
         e(i+1)=abs(xr-xu);
     end
     i=1+i;
-    
     if(fxu*fxr==0)
         xr=xr
-    end
-end
+        end
+        end
 tamanio = size(e);
 ite = 1:1:tamanio(2);
 figure('DefaultAxesFontsize', 14)

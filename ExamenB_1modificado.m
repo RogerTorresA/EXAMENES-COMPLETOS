a8b7881bde4339d62a1eeadcc6dd2ab86027f9bf
RogerTorresA <55524395+RogerTorresA@users.noreply.github.com>
2019-10-25 01:05:39 -0500

    Update ExamenB_1modificado.m

--- ExamenB_1modificado.m
+++ ExamenB_1modificado.m
@@ -14,23 +14,19 @@ while(e(i) > 0.001)
     fxl=(xl^4)-1-3*(exp(xl))
     fxr=(xr^4)-1-3*(exp(xr))
     fxu=(xu^4)-1-3*(exp(xu))
-    
     if(fxl*fxr < 0)
         xu=xr
         e(i+1)=abs(xl-xr);
     end
-    
     if(fxu*fxr < 0)
         xl=xr
         e(i+1)=abs(xr-xu);
     end
     i=1+i;
-    
     if(fxu*fxr==0)
         xr=xr
     end
-end
-
+    end
 tamanio = size(e);
 ite = 1:1:tamanio(2);
 figure('DefaultAxesFontsize', 14)

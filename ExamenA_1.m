e895e3df66e84adef0e9f28af646e32d0d38f8b6
RogerTorresA <55524395+RogerTorresA@users.noreply.github.com>
2019-10-25 01:01:45 -0500

    Update ExamenA_1.m

--- ExamenA_1.m
+++ ExamenA_1.m
@@ -5,27 +5,24 @@ e(i)= 10
  
 while(e(i) > 0.001)
     
-    xr = (xl+xu)/2
+  xr = (xl+xu)/2
     
-    fxl = (xl)-2^(- xl)
-    fxr = (xr)-2^(- xr)
-    fxu = (xu)-2^(- xu)
-    
-    if (fxl*fxr <  0)
+ fxl = (xl)-2^(- xl)
+ fxr = (xr)-2^(- xr)
+ fxu = (xu)-2^(- xu)
+if (fxl*fxr <  0)
         xu=xr
         e(i + 1 )= abs (xl-xr);
     end
-    
-    if(fxu*fxr<  0)
+  if(fxu*fxr<  0)
         xl = xr
         e(i+1)= abs(xr-xu);
     end
     i=1+i;
-    
-    if (fxu*fxr == 0 )
+   if (fxu*fxr == 0 )
         xr=xr
+    enn
     end
-end
 tamanio = size(e);
 ite= 1:1:tamanio(2);
 
